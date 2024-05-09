4ec5a4719a0f62f73a799b67a61d345a09c63528
Melikbekyan Ashot <melikbekyan.ashot@yandex.ru>
2024-05-09 17:55:12 +0300

    Enable all models from pytorch-segmentation-models

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,5 +1,9 @@
+"""
+File contains utility functions to create model.
+"""
 import torch
-from segmentation_models_pytorch import Unet, UnetPlusPlus, DeepLabV3, PSPNet, MAnet, PAN
+from segmentation_models_pytorch import Unet, UnetPlusPlus, MAnet, Linknet, FPN, PSPNet, PAN, DeepLabV3, DeepLabV3Plus
+from segmentation_models_pytorch.base import SegmentationModel
 from segmentation_models_pytorch.losses import DiceLoss, SoftCrossEntropyLoss, JaccardLoss, FocalLoss
 from torch.optim import Adam, AdamW
 
@@ -8,23 +12,26 @@ def get_model(architecture: str, encoder_name: str, encoder_weights=None, output
     kwargs = {
         'encoder_name': encoder_name,
         'encoder_weights': encoder_weights,
-        'classes': output_classes}
-    model_mapping = {
-        'Unet': Unet,
-        'Unet++': UnetPlusPlus,
-        'MANet': MAnet,
-        'PSPNet': PSPNet,
-        'PAN': PAN,
-        'DeepLabV3': DeepLabV3
+        'classes': output_classes
     }
-    model = model_mapping.get(architecture, None)(**kwargs)
+    model_mapping = {
+        'unet': Unet,
+        'unet++': UnetPlusPlus,
+        'manet': MAnet,
+        'linknet': Linknet,
+        'fpn': FPN,
+        'pspnet': PSPNet,
+        'pan': PAN,
+        'deeplabv3': DeepLabV3,
+        'deeplabv3+': DeepLabV3Plus
+    }
+    model = model_mapping.get(architecture.lower(), None)(**kwargs)
     if model is None:
         raise KeyError(f'Model {architecture} with {encoder_name} backbone is not found!')
     return model
 
 
 def get_loss(function: str, mode: str):
-    print(mode)
     loss_mapping = {
         'dice': DiceLoss(mode=mode, from_logits=True),
         'soft_cse': SoftCrossEntropyLoss(),
@@ -48,9 +55,8 @@ def get_optimizer(name: str):
     return optimizer
 
 
-def save_model(model):
-    script = model.to_torchscript()
-    torch.jit.save(script, f'weights/{model.model.name}.pt')
+def save_model(model: SegmentationModel):
+    torch.jit.save(model, f'weights/{model.name}.pt')
 
 
 def load_model(path):
