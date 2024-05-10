7dfd95711c36460e0299fc649c0c7b9b344f3419
Melikbekyan Ashot <melikbekyan.ashot@yandex.ru>
2024-05-10 07:40:50 +0300

    Remove dependency on config.yml from plot_utils.py

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -1,16 +1,16 @@
+"""
+File contains functions to plot data.
+"""
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
 import matplotlib.patches as mpatches
 
-from utils.data_utils import get_config
-
-config = get_config()
-
 
 def plot_batch(batch):
     img, mask = batch
-    fig, axes = plt.subplots(config['BATCH_SIZE'], 3, figsize=(7, 1.5 * config['BATCH_SIZE']))
+    batch_size = img.shape[0]
+    fig, axes = plt.subplots(batch_size, 3, figsize=(7, 1.5 * batch_size))
     axes[0][0].set_title('Image')
     axes[0][1].set_title('Label')
     cmap = ListedColormap(['white', 'red'])
@@ -26,8 +26,8 @@ def plot_batch(batch):
         axes[i][1].imshow(plt_mask, alpha=0.3, cmap=local_cmap)
         axes[i][1].axis('off')
 
-        class_names = {idx: label for idx, label in IDX2LABEL_TRAIN.items()
-                       if label != '_background_'}
+        idx2label = {1: 'steatosis'}
+        class_names = {idx: label for idx, label in idx2label.items()}
         legend_patches = [mpatches.Patch(color=cmap.colors[idx], label=name)
                           for idx, name in class_names.items() if idx in np.unique(plt_mask)]
         axes[i][2].legend(handles=legend_patches, loc='right', fontsize='small')
