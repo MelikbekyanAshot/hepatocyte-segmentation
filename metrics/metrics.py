b62e45d0c2b93725f816ece5b47e56b908894db3
Melikbekyan Ashot <melikbekyan.ashot@yandex.ru>
2024-05-25 17:57:36 +0300

    Refactoring

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,10 +1,26 @@
+from dataclasses import dataclass, fields
+
 from segmentation_models_pytorch.metrics import get_stats, iou_score, f1_score
 
+from utils.data_utils import get_config
 
-def compute_metrics(output, target, mode='Val'):
-    """"""
+train_config = get_config()['TRAIN']
+MODE = train_config['LOSS']['mode']
+OUTPUT_CLASSES = train_config['MODEL']['output_classes']
+
+
+@dataclass
+class Metrics:
+    F1: float
+    IoU: float
+
+    def to_dict(self, mode):
+        return {f"{mode}/{metric.name}": getattr(self, metric.name) for metric in fields(self)}
+
+
+def compute_metrics(output, target) -> Metrics:
     target = target.round().long()
-    tp, fp, fn, tn = get_stats(output, target, mode='binary', threshold=0.5)  # TODO fix hardcode 'mode'
-    iou = iou_score(tp, fp, fn, tn, reduction="micro")
-    f1 = f1_score(tp, fp, fn, tn, reduction="micro")
-    return {f'{mode}/iou_score': iou.item(), f'{mode}/f1_score': f1.item()}
+    tp, fp, fn, tn = get_stats(output, target, mode=MODE, num_classes=OUTPUT_CLASSES)
+    iou = iou_score(tp, fp, fn, tn, reduction="macro")
+    f1 = f1_score(tp, fp, fn, tn, reduction="macro")
+    return Metrics(F1=f1, IoU=iou)
--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,9 @@
+import numpy as np
 import wandb
 from pytorch_lightning import LightningModule
 
 from metrics.metrics import compute_metrics
-from utils.data_utils import get_config, split_mask, merge_mask
+from utils.data_utils import get_config
 from utils.model_utils import get_model, get_loss, get_optimizer
 
 CONFIG = get_config()
@@ -10,6 +11,7 @@ TRAIN_CONFIG = CONFIG['TRAIN']
 MODEL_CONFIG = TRAIN_CONFIG['MODEL']
 OUTPUT_CLASSES = MODEL_CONFIG['output_classes']
 LOSS_CONFIG = TRAIN_CONFIG['LOSS']
+MODE = LOSS_CONFIG['mode']
 OPT_CONFIG = TRAIN_CONFIG['OPTIMIZER']
 LR_CONFIG = TRAIN_CONFIG['LEARNING_RATE']
 WANDB_CONFIG = CONFIG['WANDB']
@@ -22,9 +24,20 @@ class SegmentationModel(LightningModule):
         self.loss_fn = get_loss(**LOSS_CONFIG)
         self.optimizer = get_optimizer(OPT_CONFIG)
         self.test_table = None
+        self.train_epoch_loss = []
+        self.train_epoch_f1 = []
+        self.train_epoch_iou = []
+        self.val_epoch_loss = []
+        self.val_epoch_f1 = []
+        self.val_epoch_iou = []
 
     def forward(self, img):
         return self.model(img)
+
+    def on_train_epoch_start(self) -> None:
+        self.train_epoch_loss = []
+        self.train_epoch_f1 = []
+        self.train_epoch_iou = []
 
     def training_step(self, batch, batch_idx):
         image, mask = batch
@@ -32,11 +45,21 @@ class SegmentationModel(LightningModule):
         logits_mask = self.forward(image).float()
         loss = self.loss_fn(logits_mask, mask)
         prob_mask = logits_mask.sigmoid()
-        pred_mask = (prob_mask > 0.5).float()
-        metrics = compute_metrics(pred_mask, split_mask(mask, OUTPUT_CLASSES), 'Train')
-        wandb.log({"Train/Loss": loss.item()})
-        wandb.log(metrics)
+        pred_mask = prob_mask.argmax(dim=1, keepdim=True)
+        metrics = compute_metrics(pred_mask, mask)
+        self.log_batch_results(loss, metrics, 'Train')
         return loss
+
+    def on_train_epoch_end(self) -> None:
+        mean_loss = np.mean(self.train_epoch_loss)
+        mean_f1 = np.mean(self.train_epoch_f1)
+        mean_iou = np.mean(self.train_epoch_iou)
+        wandb.log({'Train/EpochLoss': mean_loss, 'Train/EpochF1': mean_f1, 'Train/EpochIoU': mean_iou})
+
+    def on_validation_epoch_start(self) -> None:
+        self.val_epoch_loss = []
+        self.val_epoch_f1 = []
+        self.val_epoch_iou = []
 
     def validation_step(self, batch, batch_idx):
         image, mask = batch
@@ -44,10 +67,15 @@ class SegmentationModel(LightningModule):
         logits_mask = self.model(image).float()
         loss = self.loss_fn(logits_mask, mask)
         prob_mask = logits_mask.sigmoid()
-        pred_mask = (prob_mask > 0.5).float()
-        metrics = compute_metrics(pred_mask, split_mask(mask, OUTPUT_CLASSES), 'Val')
-        wandb.log({"Val/Loss": loss.item()})
-        wandb.log(metrics)
+        pred_mask = prob_mask.argmax(dim=1, keepdim=True)
+        metrics = compute_metrics(pred_mask, mask)
+        self.log_batch_results(loss, metrics, 'Val')
+
+    def on_validation_epoch_end(self) -> None:
+        mean_loss = np.mean(self.val_epoch_loss)
+        mean_f1 = np.mean(self.val_epoch_f1)
+        mean_iou = np.mean(self.val_epoch_iou)
+        wandb.log({'Val/EpochLoss': mean_loss, 'Val/EpochF1': mean_f1, 'Val/EpochIoU': mean_iou})
 
     def on_test_epoch_start(self) -> None:
         self.test_table = wandb.Table(columns=['sample'])
@@ -58,10 +86,10 @@ class SegmentationModel(LightningModule):
         logits_mask = self.model(image).float()
         loss = self.loss_fn(logits_mask, mask)
         prob_mask = logits_mask.sigmoid()
-        pred_mask = (prob_mask > 0.5).float()
-        metrics = compute_metrics(pred_mask, split_mask(mask, OUTPUT_CLASSES), 'Test')
+        pred_mask = prob_mask.argmax(dim=1, keepdim=True)
+        metrics = compute_metrics(pred_mask, mask)
         wandb.log({'Test/Loss': loss.item()})
-        wandb.log(metrics)
+        wandb.log(metrics.to_dict('Test'))
         self.__add_images_to_table(image, mask, pred_mask)
 
     def __add_images_to_table(self, images, masks, preds):
@@ -70,7 +98,7 @@ class SegmentationModel(LightningModule):
         for image, mask, pred in zip(images, masks, preds):
             image = image.permute(1, 2, 0).detach().cpu().numpy()
             mask = mask.detach().cpu().numpy().squeeze()
-            pred = merge_mask(pred.detach().cpu().numpy().squeeze())
+            pred = pred.detach().cpu().numpy().squeeze()
             masked_image = wandb.Image(
                 image,
                 masks={
@@ -89,8 +117,20 @@ class SegmentationModel(LightningModule):
     def predict_step(self, image, batch_idx):
         logits_mask = self.model(image.float())
         prob_mask = logits_mask.sigmoid()
-        pred_mask = (prob_mask > 0.5).float()
+        pred_mask = prob_mask.argmax(dim=1, keepdim=True)
         return pred_mask
+
+    def log_batch_results(self, loss, metrics, mode):
+        wandb.log({f"{mode}/Loss": loss.item()})
+        wandb.log(metrics.to_dict(mode=mode))
+        if mode == 'Train':
+            self.train_epoch_loss.append(loss.item())
+            self.train_epoch_f1.append(metrics.F1)
+            self.train_epoch_iou.append(metrics.IoU)
+        elif mode == 'Val':
+            self.val_epoch_loss.append(loss.item())
+            self.val_epoch_f1.append(metrics.F1)
+            self.val_epoch_iou.append(metrics.IoU)
 
     def configure_optimizers(self):
         optimizer = self.optimizer(
