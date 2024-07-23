21c268228b4a6a70834d84a51fa86c9b85c8082f
Melikbekyan Ashot <melikbekyan.ashot@yandex.ru>
2024-07-24 00:25:51 +0300

    Compute precision and recall

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass, fields
 
-from segmentation_models_pytorch.metrics import get_stats, iou_score, f1_score
+from segmentation_models_pytorch.metrics import get_stats, iou_score, f1_score, precision, recall, accuracy
 
 
 @dataclass
 class Metrics:
     F1: float
     IoU: float
+    precision: float
+    recall: float
 
     def to_dict(self, mode):
         return {f"{mode}/{metric.name}": getattr(self, metric.name) for metric in fields(self)}
@@ -15,6 +17,8 @@ class Metrics:
 def compute_metrics(output, target, mode: str, num_classes: int) -> Metrics:
     target = target.round().long()
     tp, fp, fn, tn = get_stats(output, target, mode=mode, num_classes=num_classes)
-    iou = iou_score(tp, fp, fn, tn, reduction="macro")
-    f1 = f1_score(tp, fp, fn, tn, reduction="macro")
-    return Metrics(F1=f1, IoU=iou)
+    iou = iou_score(tp, fp, fn, tn, reduction="macro").item()
+    f1 = f1_score(tp, fp, fn, tn, reduction="macro").item()
+    precision_score = precision(tp, fp, fn, tn).item()
+    recall_score = recall(tp, fp, fn, tn).item()
+    return Metrics(F1=f1, IoU=iou, precision=precision_score, recall=recall_score)
